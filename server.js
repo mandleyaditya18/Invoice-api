b6918521f583bd24989946e0fd0fba7c0895e95d
Aditya Mandley <mandleyaditya18@gmail.com>
2021-07-21 12:34:31 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+
+const DB = process.env.DB_HOST;
+mongoose.connect(DB, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        console.log('Connection Open!!!')
+    })
+    .catch(err => {
+        console.log(`Mongo Error: ${err}`)
+    })
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('Connection successful');
+})
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Serving on port ${PORT}`);
+})
