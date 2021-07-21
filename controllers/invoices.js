70993da0c062e180b6513f997a9c1c7c6123a8f6
Aditya Mandley <mandleyaditya18@gmail.com>
2021-07-21 16:40:07 +0530

    Added invoice controller

---
+++ controllers/invoices.js
@@ -0,0 +1,7 @@
+module.exports.index = async (req, res) => {
+    res.send('Index function in invoices controller');
+}
+
+module.exports.createInvoice = async (req, res) => {
+    res.send('Create Invoice function in invoices controller');
+}
