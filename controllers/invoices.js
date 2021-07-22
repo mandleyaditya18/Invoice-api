f2f12f7ac5f3a09cffe80a86f759775fe39b8d7b
Aditya Mandley <mandleyaditya18@gmail.com>
2021-07-22 23:49:09 +0530

    Updated Controller

--- controllers/invoices.js
+++ controllers/invoices.js
@@ -1,7 +1,110 @@
+const Invoice = require('../models/invoice');
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_ID,
+      pass: process.env.EMAIL_PASS
+    }
+})
+
 module.exports.index = async (req, res) => {
-    res.send('Index function in invoices controller');
+    try {
+        const invoices = await Invoice.find({});
+        res.send(invoices);
+    }
+    catch (err) {
+        const error = {error: err};
+        res.send(error);
+    }
 }
 
 module.exports.createInvoice = async (req, res) => {
-    res.send('Create Invoice function in invoices controller');
+    try {
+        const dueDate = new Date() + 1*60000;
+        const {
+            invoiceNo, orgName,
+            orgAddress, orgContact, name, 
+            email, address, phone, work,
+            expenses, total, paymentMode,
+            status, lateFeeRate, notes
+        } = req.body;
+
+        work.amount = work.hours * work.rate;
+
+        const newInvoice = await Invoice.create({
+            invoiceNo, orgName, orgAddress, 
+            orgContact, name, email, address, 
+            phone, work, expenses, total, paymentMode,
+            dueDate, status, lateFeeRate, notes
+        })
+        console.log(typeof(due));
+
+        const mailOptions = {
+            from: process.env.EMAIL_ID,
+            to: `${newInvoice.email}`,
+            subject: 'Invoice',
+            text: `${newInvoice}`
+        };
+
+        transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+            console.log(error);
+            } else {
+            console.log('Email sent: ' + info.response);
+            sent = true;
+            }
+        });
+
+        res.send(newInvoice);
+    }
+
+    catch (err) {
+        const error = {error: err};
+        res.send(error);
+    }
+}
+
+module.exports.showInvoice = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const invoice = await Invoice.findById(id);
+        res.send(invoice);
+    }
+    catch (err) {
+        const error = {error: err};
+        res.send(error);
+    }
+}
+
+module.exports.updateInvoice = async (req, res) => {
+    try{
+        const { id } = req.params;
+        const value = req.body;
+        const invoice = await Invoice.findByIdAndUpdate(id, value, {new: true})
+        await invoice.save();
+
+        const mailOptions = {
+            from: process.env.EMAIL_ID,
+            to: `${invoice.email}`,
+            subject: 'Updated Invoice',
+            text: `${invoice}`
+        };
+
+        transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+            console.log(error);
+            } else {
+            console.log('Email sent: ' + info.response);
+            sent = true;
+            }
+        });
+
+        res.send(invoice);
+    }
+    catch(err) {
+        const error = {error: err};
+        res.send(error);
+    }
 }
